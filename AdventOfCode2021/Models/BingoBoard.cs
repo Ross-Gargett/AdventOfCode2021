2fdd182784517ddf870466805b1a4f17b3e2e6d7
Ross Gargett <ross.gargett@ncontracts.com>
2021-12-04 22:24:33 -0800

    day 4 part 1 & 2

---
+++ AdventOfCode2021/Models/BingoBoard.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventOfCode2021.Models;
+
+public class BingoBoard
+{
+    private readonly int _boardDimensions;
+    private readonly bool[,] _checkedBoard;
+    private readonly int[,] _board;
+    private readonly Dictionary<int, (int, int)> _boardPositions;
+
+    public bool HasBingo { get; private set; }
+
+    public BingoBoard(IEnumerable<string> board, int boardDimensions)
+    {
+        _boardDimensions = boardDimensions;
+        _checkedBoard = new bool[_boardDimensions, _boardDimensions];
+        _board = new int[_boardDimensions, _boardDimensions];
+        _boardPositions = new Dictionary<int, (int, int)>();
+
+        SetupBoard(board);
+    }
+
+    private void SetupBoard(IEnumerable<string> board)
+    {
+        for (var i = 0; i < board.Count(); i++)
+        {
+            var cols = board.ElementAt(i).Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (var j = 0; j < cols.Length; j++)
+            {
+                var value = Convert.ToInt32(cols[j]);
+                _checkedBoard[i, j] = false;
+                _boardPositions.Add(value, (i, j));
+                _board[i, j] = value;
+            }
+        }
+    }
+
+    public void PickNumber(int number)
+    {
+        if (!_boardPositions.ContainsKey(number) || HasBingo)
+            return;
+
+        var (row, col) = _boardPositions[number];
+        _checkedBoard[row, col] = true;
+
+        CheckHasBingo(col, row);
+    }
+
+    internal void CheckHasBingo(int column, int row)
+    {
+        HasBingo = IsColumnChecked(column);
+        HasBingo |= IsRowChecked(row);
+    }
+
+    public bool IsColumnChecked(int columnNumber)
+    {
+        return Enumerable.Range(0, _checkedBoard.GetLength(0))
+            .Select(x => _checkedBoard[x, columnNumber]).All(x => x);
+    }
+
+    public bool IsRowChecked(int rowNumber)
+    {
+        return Enumerable.Range(0, _checkedBoard.GetLength(1))
+            .Select(x => _checkedBoard[rowNumber, x]).All(x => x);
+    }
+
+    public int[] GetUnmarkedNumbers()
+    {
+        var unmarked = new List<int>();
+
+        for (var i = 0; i < _boardDimensions; i++)
+        {
+            for (var j = 0; j < _boardDimensions; j++)
+            {
+                if(!_checkedBoard[i, j])
+                    unmarked.Add(_board[i, j]);
+            }
+        }
+
+        return unmarked.ToArray();
+    }
+}
---
+++ AdventOfCode2021/Services/Solvers/ProblemSolverDay4Part1.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using AdventOfCode2021.Contracts.Services;
+using AdventOfCode2021.Models;
+
+namespace AdventOfCode2021.Services.Solvers;
+
+internal class ProblemSolverDay4Part1 : IProblemSolver
+{
+    private const int BINGO_DIMENSIONS = 5;
+    private readonly List<int> _bingoInput = new List<int>();
+    private readonly List<BingoBoard> _bingoBoards = new List<BingoBoard>();
+
+    public ProblemSolverDay4Part1(IEnumerable<string> input)
+    {
+        _bingoInput = input.First().Split(',').Select(int.Parse).ToList();
+
+        var boards = input.Skip(2);
+
+        for (var i = 0; i < boards.Count(); i+=6)
+        {
+            _bingoBoards.Add(new BingoBoard(boards.Skip(i).Take(BINGO_DIMENSIONS), BINGO_DIMENSIONS));
+        }
+    }
+
+    public string Solve()
+    {
+        return $"{CalculateFinalScore()}";
+    }
+
+    private int CalculateFinalScore()
+    {
+        var unmarkedNums = new int[0];
+        var lastNum = int.MinValue;
+
+        foreach (var number in _bingoInput)
+        {
+            foreach (var board in _bingoBoards)
+            {
+                board.PickNumber(number);
+
+                if (!board.HasBingo) continue;
+
+                unmarkedNums = board.GetUnmarkedNumbers();
+                lastNum = number;
+
+                break;
+            }
+
+            if (lastNum != int.MinValue)
+                break;
+        }
+
+        return lastNum * unmarkedNums.Sum();
+    }
+}
