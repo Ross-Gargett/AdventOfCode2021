5b4bfab5fa54b879e8a4a14dcd8fb1bdcf342292
Ross Gargett <ross.gargett@ncontracts.com>
2021-12-12 10:44:24 +0000

    day 7 part 1 & 2

---
+++ AdventOfCode2021/Services/Solvers/ProblemSolverDay7Part1.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2021.Contracts.Services;
+using AdventOfCode2021.Models;
+
+namespace AdventOfCode2021.Services.Solvers;
+
+internal class ProblemSolverDay7Part1 : IProblemSolver
+{
+    private readonly List<int> _crabs;
+
+    public ProblemSolverDay7Part1(IEnumerable<string> input)
+    {
+        var crabStrs = input.First().Split(',');
+        _crabs = crabStrs.Select(int.Parse).ToList();
+    }
+
+    public string Solve()
+    {
+        return $"{FindOptimalPosition()}";
+    }
+
+    private int FindOptimalPosition()
+    {
+        return _crabs.Select(crab => _crabs.Sum(x => Math.Abs(crab - x))).Min();
+    }
+}
---
+++ AdventOfCode2021/Services/Solvers/ProblemSolverDay7Part2.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2021.Contracts.Services;
+using AdventOfCode2021.Models;
+
+namespace AdventOfCode2021.Services.Solvers;
+
+internal class ProblemSolverDay7Part2 : IProblemSolver
+{
+    private readonly List<int> _crabs;
+
+    public ProblemSolverDay7Part2(IEnumerable<string> input)
+    {
+        var crabStrs = input.First().Split(',');
+        _crabs = crabStrs.Select(int.Parse).ToList();
+    }
+
+    public string Solve()
+    {
+        return $"{FindOptimalPosition()}";
+    }
+
+    private int FindOptimalPosition()
+    {
+        return Enumerable.Range(_crabs.Min(), _crabs.Max() - _crabs.Min())
+            .Select(crab => _crabs.Sum(x => Enumerable.Range(1, Math.Abs(crab - x)).Sum()))
+            .Min();
+    }
+}
